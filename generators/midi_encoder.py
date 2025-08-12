bc22aa6120b8f7ecad52e72da66d33b46d564a72
HicSvntLeones <danielanthonysymonds@gmail.com>
2025-08-13 01:01:01 +0900

    Completed rudamentary MidiEncoder

--- generators/midi_encoder.py
+++ generators/midi_encoder.py
@@ -1,4 +1,7 @@
 from utils.midilogger import *
+from utils.midi_gen_funcs import int_to_hex
+from utils.midi_gen_funcs import int_to_VLQ
+from math import log2
 
 # This takes a list of events sorted by delta_time from the MetaEncoder, turns them into valid MIDI data and encodes into a midi.
 
@@ -12,8 +15,206 @@ class MidiEncoder:
         self.event_lists = event_lists
         self.metadata = metadata
         self.hexdata = []
+        self.current_event = None
+        self.current_track = None
+        self.current_time = 0
         stamp(4, f"MidiEncoder setup complete.")
         stamp(5, f"List of Events:{self.event_lists} across {self.metadata['track_count']+1} tracks.")
+        stamp(5, f"Metadata:{self.metadata}")
 
     def main(self):
-        pass
+        self.events_to_hex()
+        stamp(4, f"Hex data completed: {self.hexdata}")
+
+    def events_to_hex(self):
+        stamp(3, "Starting hex encoding...")
+        for i in range(0, self.metadata['track_count']+1):
+            self.encode_track(self.event_lists[i], i)
+
+    def encode_track(self, event_list, track_no):
+        stamp(4, f"Encoding track {track_no}...")
+        stamp(5, f"... from list: {event_list}")
+        self.current_track = track_no
+        for event in event_list:
+            self.current_event = event
+            self.encode_event()
+        stamp(5, f"Encoded track {track_no} with {self.hexdata[track_no]}")
+
+
+    def encode_event(self):
+        stamp(4, f"Encoding event type: {self.current_event[0]}")
+        match self.current_event[0]:
+            case "Control Change":
+                self.handle_Control_Change()
+            case "End Track":
+                self.handle_End_Track()
+            case "MThd":
+                self.handle_event_MThd()
+            case "MTrk":
+                self.handle_event_MTrk()
+            case "Note On":
+                self.handle_event_Note_On()
+            case "Note Off":
+                self.handle_event_Note_Off()
+            case "Program Change":
+                self.handle_event_Program_Change()
+            case "Set Tempo":
+                self.handle_event_Set_Tempo()
+            case "Set Time Signature":
+                self.handle_event_Set_Time_Signature()
+            case _:
+                stamp(2, f"Unhandled event in encode_event {self.current_event}")
+
+
+    def handle_Control_Change(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        delta_time = self.delta_delta_time()
+        hex = self.hexlist_append(hex, delta_time)
+        status_byte = "B" + str(format(self.current_event[2], 'X'))
+        controller = int_to_hex(self.current_event[3])
+        value = int_to_hex(self.current_event[4])
+        hex = self.hexlist_append(hex, status_byte)
+        hex = self.hexlist_append(hex, controller)
+        hex = self.hexlist_append(hex, value)
+        self.hexdata[self.current_track] += hex
+        stamp(5, f"Adding {hex} to sub-list of track {self.current_track}")
+
+    def handle_End_Track(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        END_TRACK = "FF2F00"
+        hex = self.hexlist_append(hex, END_TRACK)
+        self.hexdata[self.current_track] += hex
+        self.replace_MTrk_length()
+        
+    def handle_event_MThd(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        MIDI_HEADER = "4D546864"
+        HEADER_LENGTH = "00000006"
+        FORMAT_TYPE = "0001" #Probably won't support other format types unless I get a reason to do so.
+        track_count = int_to_hex(self.current_event[3]+1, 2) #Plus one because MIDI does not zero-index track count.
+        division = int_to_hex(self.current_event[4], 2) #Only supports PPQN. Probably won't support SMPTE unless I get a reason to do so.
+        hex = self.hexlist_append(hex, MIDI_HEADER)
+        hex = self.hexlist_append(hex, HEADER_LENGTH)
+        hex = self.hexlist_append(hex, FORMAT_TYPE)
+        hex = self.hexlist_append(hex, track_count)
+        hex = self.hexlist_append(hex, division)
+        stamp(5, f"Appending {hex} to new sub-list of track 0") # Because MThd is always track 0, and always the first event.
+        self.hexdata.append(hex)
+
+    def handle_event_MTrk(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        TRACK_HEADER = "4D54726B"
+        TEMP_LENGTH = "ZZZZZZZZ" #This is a temporary (and invalid) length that will be easy to find later.
+        hex = self.hexlist_append(hex, TRACK_HEADER)
+        hex = self.hexlist_append(hex, TEMP_LENGTH)
+        if self.current_track == 0: # If track 0, MTrk is not the start of the event list. On all other tracks, it is.
+            stamp(5, f"Adding {hex} to track 0") 
+            self.hexdata[0] += hex
+            print(self.hexdata)
+            return
+        self.hexdata.append(hex)
+        stamp(5, f"Adding {hex} to new sub-list of track {self.current_track}") 
+
+    def handle_event_Note_On(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        delta_time = self.delta_delta_time()
+        hex = self.hexlist_append(hex, delta_time)
+        status_byte = "9" + str(format(self.current_event[2], 'X'))
+        pitch = int_to_hex(self.current_event[3])
+        velocity = int_to_hex(self.current_event[4])
+        hex = self.hexlist_append(hex, status_byte)
+        hex = self.hexlist_append(hex, pitch)
+        hex = self.hexlist_append(hex, velocity)
+        self.hexdata[self.current_track] += hex
+        stamp(5, f"Adding {hex} to sub-list of track {self.current_track}") 
+
+    def handle_event_Note_Off(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        delta_time = self.delta_delta_time()
+        hex = self.hexlist_append(hex, delta_time)
+        status_byte = "8" + str(format(self.current_event[2], 'X'))
+        pitch = int_to_hex(self.current_event[3])
+        velocity = int_to_hex(self.current_event[4])
+        hex = self.hexlist_append(hex, status_byte)
+        hex = self.hexlist_append(hex, pitch)
+        hex = self.hexlist_append(hex, velocity)
+        self.hexdata[self.current_track] += hex
+        stamp(5, f"Adding {hex} to sub-list of track {self.current_track}") 
+        
+    def handle_event_Program_Change(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        delta_time = self.delta_delta_time()
+        hex = self.hexlist_append(hex, delta_time)
+        status_byte = "C" + str(format(self.current_event[2], 'X'))
+        program = int_to_hex(self.current_event[3])
+        hex = self.hexlist_append(hex, status_byte)
+        hex = self.hexlist_append(hex, program)
+        self.hexdata[self.current_track] += hex
+        stamp(5, f"Adding {hex} to sub-list of track {self.current_track}") 
+
+    def handle_event_Set_Tempo(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        delta_time = self.delta_delta_time()
+        hex = self.hexlist_append(hex, delta_time)
+        SET_TEMPO = "FF5103"
+        tempo = int_to_hex(60000000/self.current_event[2],3)
+        hex = self.hexlist_append(hex, SET_TEMPO)
+        hex = self.hexlist_append(hex, tempo)
+        self.hexdata[self.current_track] += hex
+        stamp(5, f"Adding {hex} to sub-list of track {self.current_track}") 
+
+    def handle_event_Set_Time_Signature(self):
+        stamp(5, f"... with parameters {self.current_event[1:]}")
+        hex = []
+        delta_time = self.delta_delta_time()
+        hex = self.hexlist_append(hex, delta_time)
+        TIME_SIGNATURE = "FF5804"
+        numerator = int_to_hex(self.current_event[2], 1) 
+        denominator = int_to_hex(int(log2(self.current_event[3])), 1)
+        clocks_per_tick = int_to_hex(self.current_event[4], 1)
+        thirtyseconds_per_quarter = int_to_hex(self.current_event[5], 1)
+        hex = self.hexlist_append(hex, TIME_SIGNATURE)
+        hex = self.hexlist_append(hex, numerator)
+        hex = self.hexlist_append(hex, denominator)
+        hex = self.hexlist_append(hex, clocks_per_tick)
+        hex = self.hexlist_append(hex, thirtyseconds_per_quarter)
+        self.hexdata[self.current_track] += hex
+        stamp(5, f"Adding {hex} to sub-list of track {self.current_track}") 
+
+
+    def hexlist_append(self, array, string):
+        if len(string)%2 != 0:
+            stamp(2, "Invalid hex string.")
+            return
+        string_array = array + [string[i:i+2] for i in range(0, len(string), 2)]
+        stamp(5, f"Updated {array} to {string_array}")
+        return string_array
+    
+    def delta_delta_time(self):
+        delta_time = self.current_event[1] - self.current_time
+        self.current_time+=delta_time
+        delta_time = int_to_VLQ(delta_time)
+        return delta_time
+    
+    def replace_MTrk_length(self):
+        stamp(5, f"Replacing length of track {self.current_track}")
+        track_length = len(self.hexdata[self.current_track]) - 8
+        plusheader = 0
+        if self.current_track == 0:
+            track_length -= 14 #Ignore the MIDI Header on track 0
+            plusheader = 14
+        stamp(5, f"Current track header {self.hexdata[self.current_track][0+plusheader:8+plusheader]}")
+        hex_length = int_to_hex(track_length, 4)
+        self.hexdata[self.current_track][4+plusheader:8+plusheader] = [hex_length[i:i+2] for i in range(0, len(hex_length), 2)]
+        stamp(5, f"Appended track header {self.hexdata[self.current_track][0+plusheader:8+plusheader]}")
+
+        
+        
--- utils/midi_gen_funcs.py
+++ utils/midi_gen_funcs.py
@@ -78,3 +78,22 @@ def get_pitch(note):
                 stamp(2, f"Invalid note received: {note}")
                 return 0
     
+def int_to_hex(num, hex_length = 1):
+    stamp(5, f"Converting {num} to {hex_length} hex(es)")
+    stringed_length = str(hex_length*2)
+    hex_string = f"{int(num):0{stringed_length}X}"
+    stamp(5, f"Result: {hex_string}")
+    return hex_string
+
+def int_to_VLQ(num):
+    stamp(5, f"Converting {num} to VLQ hex(es)")
+    vlq = [num & 0x7F] #Bitwise AND operation that leaves just the final 7 bits
+    num >>= 7 #Shifts num 7 bits to the right (bits shifted past 0 are removed)
+    while num:
+        next_byte = (num & 0x7F) | 0x80 #Leaves just the final 7 bits, then a bitwise OR adds bit 8
+        vlq.insert(0, next_byte) #inserts in byte list at position 0, shifts the rest right
+        num >>= 7
+    stamp(5, f"Resulted in {vlq}")
+    vlq_hex = [format(item, '02X') for item in vlq]
+    stamp(5, f"Converted to hex: {vlq_hex}")
+    return ''.join(vlq_hex)
